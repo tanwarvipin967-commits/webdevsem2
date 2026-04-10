45640413c3425945d34f88adcabd1f46767c4cb4
Vipin Tanwar <tanwarvipin967@gmail.com>
2026-04-10 17:42:49 +0530

    Lecture 26

--- reactapp/src/component/Article.jsx
+++ reactapp/src/component/Article.jsx
@@ -23,58 +23,62 @@
 
 import React from 'react'
 import { add as addition } from '../utility'
+import style from '../css/Article.module.css'
+import style1 from '../css/HomePage.module.css'
 
 class Article extends React.Component {
-    constructor(props) {
-      super(props)
-      this.state={
-        count:0,
-        name:"Alex"
-      }
+  constructor(props) {
+    super(props)
+    this.state = {
+      count: 0,
+      name: "Alex"
+    }
     //   console.log(addition(2,3))
-    }
+  }
 
-    componentDidMount(){
-        console.log("componentDidMount")
-    }
+  componentDidMount() {
+    console.log("componentDidMount")
+  }
 
-    shouldComponentUpdate(nextProps, nextState){
-      if(nextState.count===1)return false
-        console.log("shouldComponentUpdate")
-        return true
-    }
+  shouldComponentUpdate(nextProps, nextState) {
+    if (nextState.count === 1) return false
+    console.log("shouldComponentUpdate")
+    return true
+  }
 
 
-    componentDidUpdate(){
-        console.log("componentDidUpdate")
-    }
+  componentDidUpdate() {
+    console.log("componentDidUpdate")
+  }
 
-    componentWillUnmount(){
-        console.log("componentWillUnmount")
-    }
+  componentWillUnmount() {
+    console.log("componentWillUnmount")
+  }
 
-    handleClick=()=>{
-        // this.state.count=this.state.count+1
-        // console.log(this.state.count)
-        this.setState({count: this.state.count+1})
-    }
+  handleClick = () => {
+    // this.state.count=this.state.count+1
+    // console.log(this.state.count)
+    this.setState({ count: this.state.count + 1 })
+  }
 
-    handleDecrement=()=>{
-        this.setState({count: this.state.count-1})
-    }
+  handleDecrement = () => {
+    this.setState({ count: this.state.count - 1 })
+  }
   render() {
     return (
       <div>
         <h1>Article</h1>
         <h3>{this.state.count}</h3>
-        <button onClick={this.handleClick}>Increment</button>
-        <button onClick={this.handleDecrement}>Decrement</button>
-        <h3>{this.props.title}</h3> 
+        <button className={style.btn} onClick={this.handleClick}>Increment</button>
+        <br />
+        <br />
+        <button className={style1.btn} onClick={this.handleDecrement}>Decrement</button>
+        <h3>{this.props.title}</h3>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi eligendi fugiat pariatur eos quibusdam ratione possimus adipisci, impedit, autem ad quo, et cum laudantium deleniti. Soluta molestias amet quas labore.</p>
       </div>
     )
   }
 }
 
-export default  Article
+export default Article
 
